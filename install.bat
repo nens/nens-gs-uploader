52206eecd9c5ecdb7260d7782b918386d554d896
Ckerklaan <38913567+Ckerklaan@users.noreply.github.com>
2020-02-10 11:46:06 +0100

    added conda forge

--- install.bat
+++ install.bat
@@ -1,10 +1,7 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 echo off
 call "%~dp0install_environment.bat"
-call C:\Users\%username%\AppData\Local\Continuum\anaconda3\Scripts\activate.bat 
 echo activate catalogue
 call activate catalogue
-REM echo install install gsconfig
-REM call pip install gsconfig-py3==1.0.7
 echo Finished installing catalogue environment
 cd /D "%~dp0"
